0db8d0a6eba915222dbede45b784b843563aba60
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-29 00:37:15 +0100

    basic actions and views for pins and boards, but still disconnected

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,29 @@
 class BoardsController < ApplicationController
+	before_action :set_board, only: [:show]
+
+	def index
+		@boards = Board.all
+	end
+
+	def new
+		@board = Board.new
+	end
+
+	def create
+		@board = Board.new(board_params)
+		if @board.save
+			redirect_to @board
+		else
+			render :new
+		end
+	end
+
+	private
+		def set_board
+			@board = Board.find(params[:id])
+		end
+
+		def board_params
+			params.require(:board).permit(:title, :description, :img, :color)
+		end	
 end
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,33 @@
 class PinsController < ApplicationController
+	before_action :set_pin, only: [:show]
+
 	def index
 		@pins = Pin.all
 	end
+
+	def new
+		@pin = Pin.new
+	end
+
+	def create
+		@pin = Pin.new(pin_params)
+		if @pin.save
+			redirect_to @pin
+		else
+			render :new
+		end
+	end
+
+	def show
+		
+	end
+
+	private
+		def set_pin
+			@pin = Pin.find(params[:id])
+		end
+
+		def pin_params
+			params.require(:pin).permit(:title, :url, :notes, :imgs)
+		end
 end
