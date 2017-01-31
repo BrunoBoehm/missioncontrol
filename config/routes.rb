79b5ece1a48e05fb214603ae2ef2a0dd819bb2df
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-31 19:43:56 +0100

    added a homepage and container to all views to enable full width

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :pins, only: [:new]
   end
 
-  root "pins#index"
+  root "static_pages#home"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
