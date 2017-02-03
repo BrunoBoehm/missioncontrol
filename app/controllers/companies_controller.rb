ae89d89c80eed75d34ae12641603994c6b6a2ce9
BrunoBoehm <bruno.boehm@gmail.com>
2017-02-04 00:02:19 +0100

    added destroy action and list of related companies in add_company partial

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,4 +2,10 @@ class CompaniesController < ApplicationController
 	def index
 		@companies = Company.all
 	end
+
+	def destroy
+		company = Company.find(params[:id])
+		company.destroy
+		redirect_to :back, notice: "Company was deleted"
+	end
 end
