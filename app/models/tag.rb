e46f1c8eaf873739739115c5c22f33f83fc4c966
BrunoBoehm <bruno.boehm@gmail.com>
2017-02-05 00:22:36 +0100

    improved the tags index page and changed to delete_all for tags dependent taggings

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag < ActiveRecord::Base
-	has_many :taggings, dependent: :destroy
+	has_many :taggings, dependent: :delete_all
 	has_many :pins, through: :taggings, source: :taggable, source_type: 'Pin'
+	has_many :companies, through: :taggings, source: :taggable, source_type: 'Company'
+	has_many :people, through: :taggings, source: :taggable, source_type: 'Person'
 
 	validates_uniqueness_of :name, case_sensitive: false
 	
