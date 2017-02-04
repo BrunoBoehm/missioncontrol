ac528d9210ca247a70dfcfcc7ea897ebeac11b02
BrunoBoehm <bruno.boehm@gmail.com>
2017-02-05 00:09:23 +0100

    added an edit form for people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,37 @@
 class PeopleController < ApplicationController
+	before_action :set_person, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@people = Person.all
 	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @person.update(person_params)
+			redirect_to @person, notice: "Well done"
+		else
+			render :edit, notice: "Couldn't be updated"
+		end
+	end
+
+	def destroy
+		@person.destroy
+		redirect_to people_path, notice: "Person deleted"
+	end
+
+	private
+		def set_person
+			@person = Person.find(params[:id])
+		end
+
+		def person_params
+			params.require(:person).permit(:name, :surname, :img)
+		end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,13 @@
 class Person < ActiveRecord::Base
-	has_many :perso_links
+	has_many :perso_links, dependent: :delete_all
 	has_many :pins, through: :perso_links
 	has_many :staffings
 	has_many :companies, through: :staffings
 	has_one :social_profile
 
 	default_scope { where(account_id: Account.current_id) }
+
+	def full_name
+		name + " " + surname
+	end
 end
