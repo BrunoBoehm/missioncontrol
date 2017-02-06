d3001ac25315596f0381131445ddd48f42d5c1c5
BrunoBoehm <bruno.boehm@gmail.com>
2017-02-06 22:41:28 +0100

    enabled vimeo embed

---
+++ app/assets/javascripts/pins.js
@@ -0,0 +1,25 @@
+$(function(){
+	initTinyMCE();
+});
+
+$(window).on('page:load', function() {
+  initTinyMCE();
+});
+
+function initTinyMCE() {
+	tinymce.init({ 
+	  selector:'textarea.editor',
+	  menubar: false,
+	  plugins: [
+	  'autolink lists link preview',
+	  'table contextmenu paste code'
+	  ],
+	  statusbar: false,
+	  style_formats: [
+	    {title: "Bold", icon: "bold", format: "bold"},
+	    {title: "Italic", icon: "italic", format: "italic"},
+	    {title: "Blockquote", format: "blockquote"}
+	  ],
+	  toolbar: 'removeformat undo redo preview code | styleselect bold italic table | bullist numlist outdent indent | link'
+	});
+}
--- app/helpers/pins_helper.rb
+++ app/helpers/pins_helper.rb
@@ -9,15 +9,43 @@ module PinsHelper
 	end
 
 	def is_video?(url)
-		url.scan(/youtube/i) != [] ? true : false
+		if is_youtube_video?(url)
+			video_from_yt_url(url)
+		elsif is_vimeo_video?(url)
+			video_from_vimeo_url(url)
+		else
+			false
+		end
 	end
 
-	def video_from_url(url)
+	def is_youtube_video?(url)
+		!!!url.scan(/youtube\.com/i).empty?
+	end
+
+	def is_vimeo_video?(url)
+		!!!url.scan(/vimeo\.com/i).empty?
+	end	
+
+	def video_from_yt_url(url)
+		src = url.split("?v=").last
+		"https://www.youtube.com/embed/" + src + "?rel=0&amp;showinfo=0"
+	end
+
+	def youtube_ref_from_url(url)
 		url.split("?v=").last
 	end
 
+	def video_from_vimeo_url(url)
+		src = url.split("/").last
+		"https://player.vimeo.com/video/" + src
+	end
+
+	def vimeo_thumnail(url)
+		# no easy way without API.
+	end
+
 	def youtube_thumbnail(url)
-		"https://i.ytimg.com/vi/#{video_from_url(url)}/maxresdefault.jpg"
+		"https://i.ytimg.com/vi/#{youtube_ref_from_url(url)}/maxresdefault.jpg"
 	end
 
 	def clean(text)
